d1c3254f40eea6bd825e5c871d5055ffa64004cb
WintonAutos <35133014+WintonAutos@users.noreply.github.com>
2019-03-03 01:24:29 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 function vr(){
-	var random = Math.floor(Math.random()*2) + 1;
+	var random = Math.floor(Math.random()*1) + 1;
 	
 	if(random == 1){
-	window.location.href = "";
+	window.location.href = "https://soundcloud.com/user-707679361/vapor-radio-ep-1";
 	}
 	if(random == 2){
 	window.location.href="";	
